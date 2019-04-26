b8d618b507128f8d70f61f37cfe631efe8e30eb2
ludemeula <ludemeula@gmail.com>
2019-04-26 17:41:10 -0300

    Adicionando produtos a pagina inicial

--- src/pages/main.js
+++ src/pages/main.js
@@ -7,6 +7,10 @@ export default class Main extends Component {
     title: 'JSHunt'
   };
 
+  state = {
+    docs: [],
+  };
+
   componentDidMount() {
     this.loadProducts();
   };
@@ -15,13 +19,15 @@ export default class Main extends Component {
     const response = await api.get('/products');
 
     const { docs } = response.data;
-    console.log(docs)
+
+    this.setState({ docs })
   };
 
   render() {
     return (
       <View>
         <Text>Página Main</Text>
+        {this.state.docs.map(product => <Text key={product._id}>{product.title}</Text>)}
       </View>
     );
   };
